c6bcf52a7cbc926e3b7d6b0b1c2c0c84e2d224b7
Camille Semaan <csemaan@cmaisonneuve.qc.ca>
2023-03-09 12:03:26 -0500

    Préparation du site en PHP

---
+++ inclusions/entete.inc.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link href='http://fonts.googleapis.com/css?family=Cinzel+Decorative:700,900|Roboto+Slab:300,700|Roboto:700,400' rel='stylesheet' type='text/css'>
+  <meta charset="UTF-8">
+  <meta name="robots" content="noindex, nofollow">
+  <title>Accueil | Restaurant Leila</title>
+  <meta name="description" content="Restaurant Leila - Montréal">
+  <link rel="stylesheet" href="css/leila.css">
+  <link rel="icon" type="image/png" href="images/favicon.png" />
+</head>
+<body>
+  <div id="conteneur" class="page-<?= $page; ?>">
+    <header>
+      <div class="barre-haut">
+        <nav class="social">
+          <a href="http://www.facebook.com" target="lien-externe">
+            <img alt="Facebook" src="images/iu/nav-icone-facebook.svg">
+          </a>
+          <a href="http://www.twitter.com" target="lien-externe">
+            <img alt="Twitter" src="images/iu/nav-icone-twitter.svg">
+          </a>
+        </nav>
+        <?php if($page !== 'accueil') : ?>
+          <h1 class="logo">
+            <a href="index.php">LEILA</a>
+          </h1>
+        <?php endif; ?>
+        <nav class="i18n">
+          <a href="#" class="actif" title="Français">fr</a>
+          <a href="#" title="English">en</a>
+        </nav>
+      </div>
---
+++ inclusions/pied2page.inc.php
@@ -0,0 +1,16 @@
+    <footer>
+      <h2>Info pratique</h2>
+      <p>Cuisine ouverte de 11 h à 22 h.<br>Fermé le lundi.</p>
+      <p>Pour réservation : 
+        <span class="gras">(514) 958-2580</span>
+      </p>
+      <p class="adresse">
+        <a href="https://goo.gl/maps/9pTkr" target="lien-externe" title="Cliquez ici pour localiser le restaurant sur Google Maps">
+          <img src="images/iu/nav-icone-google-maps.png" alt="Carte">
+        </a>
+        275 rue Notre-Dame Est, Montréal, Québec
+      </p>
+    </footer>
+  </div>
+</body>
+</html>
